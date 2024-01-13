57d38f1010f5637e3f0392644864ae1097fc549e
Anaclara <anaclaraferrando95@gmail.com>
2024-01-13 15:19:45 -0300

    nav y header terminados

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//**************** Show Menu ****************/
+
+//selection Dom Element
+const menu= document.getElementById('nav-menu');
+const toggle= document.getElementById('nav-toggle');
+const nav_close= document.getElementById('nav-close');
+
+//function view menu
+if(toggle){
+    toggle.addEventListener('click', ()=>{
+        menu.classList.add('show-menu');
+    });
+};
+
+//Menu hidden
+
+if(nav_close){
+ nav_close.addEventListener('click', ()=>{
+    menu.classList.remove('show-menu');
+ });
+};
+
+//**************** REMOVE MENU MOVILE ****************/
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const menu = document.getElementById('nav-menu');
+    //when we click on each nav__link, we remove the show-menu class
+
+    menu.classList.remove('show-menu');
+} 
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
