10e5d7c57f068587f498c165484c30c6d867951d
Anaclara <anaclaraferrando95@gmail.com>
2024-01-15 16:07:32 -0300

    button y blur header

--- js/main.js
+++ js/main.js
@@ -32,3 +32,15 @@ const linkAction = () => {
 } 
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/* Add blur header */
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >=50 ? header.classList.add('blur-header')
+                      : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', blurHeader)
