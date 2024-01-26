77bccc0a2b79aac9c3bf5022cb27ef000df86825
Anaclara <anaclaraferrando95@gmail.com>
2024-01-26 17:26:02 -0300

    breakpoints

--- js/main.js
+++ js/main.js
@@ -44,3 +44,39 @@ const blurHeader = () => {
 }
 
 window.addEventListener('scroll', blurHeader)
+
+/****************** Show Scroll Up ****************************/
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag withthe scrollup class 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+/*********************************************Scroll sections active link**************************************************/
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a [href*=' + sectionId +']' )
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionClass.classList.add('active-link')
+    } else {
+        sectionClass.classList.remove('active-link')
+    }
+
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
